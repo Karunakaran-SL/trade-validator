9836daa747212f040d9a95feeb2194676014353c
Karan <karunakaran.subashchandrabose@astrazeneca.com>
2018-01-20 12:33:06 +0530

    Project setup

---
+++ trade-validator-service/src/main/java/com/coding/trade/Trade/Validator/Service/TradeValidatorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.coding.trade.Trade.Validator.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeValidatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeValidatorServiceApplication.class, args);
+	}
+}
